234592342531c78a7e96061942c0940e2e05e667
Danny Nguyen <dnguyen130@my.bcit.ca>
2021-11-28 00:32:15 -0800

    created files

--- controller/delete/movies.php
+++
@@ -1,7 +0,0 @@
-<?php
-  //then make the rules for the get request
-  if(isset($_DELETE['id'])){
-    DeleteMovie($_DELETE['id']);
-    echo "deleted!";
-  }
-?>
--- controller/patch/movies.php
+++
@@ -1,8 +0,0 @@
-<?php
-  //then make the rules for the get request
-
-  if(isset($_PATCH['movie_name']) && isset($_PATCH['id'])){
-    UpdateMovie($_PATCH['movie_name'], $_PATCH['id']);
-    echo "updated!";
-  }
-?>
