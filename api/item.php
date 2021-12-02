1bf60811f05e53a5aaffb6c8c07999dac6ad964b
Danny Nguyen <dnguyen130@my.bcit.ca>
2021-12-02 07:38:48 -0800

    added ../

--- api/item.php
+++ api/item.php
@@ -4,19 +4,19 @@
   require_once("model/item.php");
 
   if($_SERVER['REQUEST_METHOD'] == 'GET'){
-    require_once("controller/get/item.php");
+    require_once("../controller/get/item.php");
   }
   
   //if you send a new movie name over under the variable movie_name, then I will add a new move to the database
   if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    require_once("controller/post/item.php");
+    require_once("../controller/post/item.php");
   }
   
   if($_SERVER['REQUEST_METHOD'] == 'PATCH'){
-    require_once("controller/patch/item.php");
+    require_once("../controller/patch/item.php");
   }
   
   if($_SERVER['REQUEST_METHOD'] == 'DELETE'){
-    require_once("controller/delete/item.php");
+    require_once("../controller/delete/item.php");
   }
   ?>
--- api/restaurant.php
+++ api/restaurant.php
@@ -4,19 +4,19 @@
   require_once("model/restaurant.php");
 
   if($_SERVER['REQUEST_METHOD'] == 'GET'){
-    require_once("controller/get/restaurant.php");
+    require_once("../controller/get/restaurant.php");
   }
   
   //if you send a new movie name over under the variable movie_name, then I will add a new move to the database
   if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    require_once("controller/post/restaurant.php");
+    require_once("../controller/post/restaurant.php");
   }
   
   if($_SERVER['REQUEST_METHOD'] == 'PATCH'){
-    require_once("controller/patch/restaurant.php");
+    require_once("../controller/patch/restaurant.php");
   }
   
   if($_SERVER['REQUEST_METHOD'] == 'DELETE'){
-    require_once("controller/delete/restaurant.php");
+    require_once("../controller/delete/restaurant.php");
   }
   ?>
