6990cdc4197a70aa0104ccf80ea9b03ac0915dd7
Danny Nguyen <dnguyen130@my.bcit.ca>
2022-01-17 15:55:41 -0800

    updated database

--- model/connect.php
+++ model/connect.php
@@ -1,7 +1,7 @@
 <?php
 //connect to the spreadsheet mdiatest
 try {
-  $db = new PDO("mysql:dbname=defaultdb;host=db-mysql-sfo2-94703-do-user-10363188-0.b.db.ondigitalocean.com:25060", "doadmin", "uletajN9CXSngTXV");
+  $db = new PDO("mysql:dbname=defaultdb;host=db-mysql-sfo2-61504-do-user-10363188-0.b.db.ondigitalocean.com:25060", "doadmin", "cJo0OrGY7ifxvnuI");
 } catch (Exception $e){
   echo $e->getMessage();
 }
